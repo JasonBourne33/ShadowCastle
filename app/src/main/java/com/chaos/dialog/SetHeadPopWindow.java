78597812a61cec0eaedbebf88478cf44239cc339
JasonBourne33 <106587387@qq.com>
2018-02-26 09:08:33 +0800

    1 完成头像更换弹窗

--- app/src/main/java/com/chaos/dialog/SetHeadPopWindow.java
+++ app/src/main/java/com/chaos/dialog/SetHeadPopWindow.java
@@ -1,18 +1,66 @@
 package com.chaos.dialog;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.drawable.ColorDrawable;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.PopupWindow;
+import android.widget.TextView;
+
+import com.example.administrator.multiplestatusviewtest.R;
+
+import static com.chaos.util.Common.TAG;
 
 /**
  * Created by chaos on 2018/2/6 0006.
  */
 
-public class SetHeadPopWindow extends PopupWindow {
+public class SetHeadPopWindow extends PopupWindow implements View.OnClickListener{
     public SetHeadPopWindow(Activity context) {
         super(context);
         LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-//        inflater.inflate(R.layout.)
+        View view = inflater.inflate(R.layout.popwindow_head, null);
+        TextView tvAbum = (TextView) view.findViewById(R.id.tv_album);
+        TextView tvTakePhoto = (TextView) view.findViewById(R.id.tv_takePhoto);
+        TextView tvCancel = (TextView) view.findViewById(R.id.tv_cancel);
+        tvAbum.setOnClickListener(this);
+        tvTakePhoto.setOnClickListener(this);
+        tvCancel.setOnClickListener(this);
+
+        // 设置SelectPicPopupWindow的View
+        this.setContentView(view);
+        // 设置SelectPicPopupWindow弹出窗体的宽
+        this.setWidth(ActionBar.LayoutParams.MATCH_PARENT);
+        // 设置SelectPicPopupWindow弹出窗体的高
+        this.setHeight(ActionBar.LayoutParams.WRAP_CONTENT);
+        // 设置SelectPicPopupWindow弹出窗体可点击
+//        this.setFocusable(true);
+        // 设置SelectPicPopupWindow弹出窗体动画效果
+        this.setAnimationStyle(R.style.AnimBottom);
+        //去掉popWindow 的默认灰色背景
+        setBackgroundDrawable(new ColorDrawable(context.getResources().getColor(R.color.transparent)));
+        // 实例化一个ColorDrawable颜色为半透明
+        ColorDrawable dw = new ColorDrawable(0xb0000000);
+        // 设置SelectPicPopupWindow弹出窗体的背景
+//        this.setBackgroundDrawable(dw);
+        // mMenuView添加OnTouchListener监听判断获取触屏位置如果在选择框外面则销毁弹出框
+
+    }
+
+//    @OnClick({R.id.tv_album, R.id.tv_takePhoto, R.id.tv_cancel})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.tv_album:
+                break;
+            case R.id.tv_takePhoto:
+                break;
+            case R.id.tv_cancel:
+                Log.e(TAG, "onClick: tv_cancel=== " );
+                this.dismiss();
+                break;
+        }
     }
 }
--- app/src/main/java/com/example/administrator/multiplestatusviewtest/SetHeadViewActivity.java
+++ app/src/main/java/com/example/administrator/multiplestatusviewtest/SetHeadViewActivity.java
@@ -3,8 +3,14 @@ package com.example.administrator.multiplestatusviewtest;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.View;
+import android.view.WindowManager;
+import android.widget.PopupWindow;
+
+import com.chaos.dialog.SetHeadPopWindow;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -12,9 +18,11 @@ import butterknife.OnClick;
 
 public class SetHeadViewActivity extends AppCompatActivity {
 
+    private static final String TAG = "e";
     @Bind(R.id.toolbar)
     Toolbar toolbar;
 
+    private SetHeadPopWindow setHeadPopWindow;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,8 +41,37 @@ public class SetHeadViewActivity extends AppCompatActivity {
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.btn_showPopWindow:
+                Log.e(TAG, "onClick:btn_showPopWindow===  " );
+                setBackgroundAlpha(0.5f);
 
+                setHeadPopWindow = new SetHeadPopWindow(this);
+                setHeadPopWindow.showAtLocation(toolbar, Gravity.BOTTOM,0,0);
+                setHeadPopWindow.setOnDismissListener(new PopupWindow.OnDismissListener() {
+                    @Override
+                    public void onDismiss() {
+                        setBackgroundAlpha(1.0f);
+                    }
+                });
                 break;
         }
+    }
+
+
+    /**
+     * 设置添加屏幕的背景透明度
+     *
+     * @param alpha
+     *            屏幕透明度0.0-1.0 1表示完全不透明
+     */
+//    public void setBackgroundAlpha(float bgAlpha) {
+//        WindowManager.LayoutParams lp = this.getWindow().getAttributes();
+//        lp.alpha = bgAlpha;
+//        this.getWindow().setAttributes(lp);
+//    }
+    public void setBackgroundAlpha(float alpha){
+        WindowManager.LayoutParams lp = this.getWindow().getAttributes();
+        lp.alpha = alpha;
+        this.getWindow().addFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND);
+        this.getWindow().setAttributes(lp);
     }
 }
